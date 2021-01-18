70b63869038517dd3d490336ec4c5f5fc90d8d5c
Matt Venn <matt@mattvenn.net>
2021-01-18 11:33:10 +0100

    notes

--- timer.v
+++ timer.v
@@ -21,12 +21,16 @@ module timer(
     assign busy = counter > 0;
 
     `ifdef FORMAL
+    // register for knowing if we have just started
     reg f_past_valid = 0;
+    // start in reset
     initial assume(reset);
     always @(posedge clk) begin
         
+        // assume timer won't get loaded with a 0
         assume(cycles > 0);
 
+        // update past_valid reg so we know it's safe to use $past()
         f_past_valid <= 1;
 
         // cover the counter getting loaded and starting to count
